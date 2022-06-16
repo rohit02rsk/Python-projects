f8b3a9cda71c0b30c0f1869d69149a62c768cb0c
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2022-06-16 14:42:55 +0530

    Update snake.py

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -21,9 +21,10 @@ class Snake:
         new_seg.goto(pos)
         self.segments.append(new_seg)
 
-    def create_snake(self):
+    def create_snake(self):  
         for pos in STARTING_POSITIONS:
             self.add_segment(pos)
+        
 
     def move(self):
         for seg in range(len(self.segments) - 1, 0, -1):
@@ -51,3 +52,11 @@ class Snake:
     
     def extend(self):
         self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+        self.head.color("chartreuse")
