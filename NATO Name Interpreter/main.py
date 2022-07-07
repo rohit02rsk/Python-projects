076463ae9d563fe65afd584a4a5e8e0638e91d3f
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2022-07-07 11:55:25 +0530

    Update main.py

--- NATO Name Interpreter/main.py
+++ NATO Name Interpreter/main.py
@@ -4,7 +4,14 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter:row.code for (ind, row) in data.iterrows()}
 print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
+def gen_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, please use only the English Lexicon.")
+        gen_phonetic()
+    else:
+        print(output_list)
 
-print(output_list)
+gen_phonetic()
