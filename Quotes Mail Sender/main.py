6a28a3e11c9740bd0d613f181b0874a8cdc2bddf
S Rohit Kumar <96235764+rohit02rsk@users.noreply.github.com>
2022-07-13 12:25:24 +0530

    Update main.py

--- Quotes Mail Sender/main.py
+++ Quotes Mail Sender/main.py
@@ -7,7 +7,8 @@ MY_PASSWORD = "*****************"
 
 now = dt.datetime.now()
 weekday = now.weekday()
-if weekday == 2:
+
+if weekday == 0:
     with open("quotes.txt") as quote_file:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
